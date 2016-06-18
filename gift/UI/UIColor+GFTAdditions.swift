2b4a5ecab48d2bf2625bc49db6d3cd225b8ac090
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-06-18 21:48:16 +0300

    Adding fonts, colors and assets

---
+++ gift/UI/UIColor+GFTAdditions.swift
@@ -0,0 +1,46 @@
+//
+//  UIColor+GFTAdditions.swift
+//
+//  Created by Matan Lachmish on 06/06/2016.
+//  Copyright © 2016 GiftApp. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	class func gftWaterBlueColor() -> UIColor {
+		return UIColor(red: 25.0 / 255.0, green: 118.0 / 255.0, blue: 210.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftWarmGreyColor() -> UIColor {
+		return UIColor(white: 114.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftWhiteColor() -> UIColor {
+		return UIColor(white: 255.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftBlackColor() -> UIColor {
+		return UIColor(white: 33.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftPowderBlueColor() -> UIColor {
+		return UIColor(red: 187.0 / 255.0, green: 222.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftTurquoiseBlueColor() -> UIColor {
+		return UIColor(red: 0.0, green: 188.0 / 255.0, blue: 212.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftGreyishColor() -> UIColor {
+		return UIColor(white: 182.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftAzureColor() -> UIColor {
+		return UIColor(red: 33.0 / 255.0, green: 150.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+	}
+
+	class func gftBackgroundWhiteColor() -> UIColor {
+		return UIColor(white: 247.0 / 255.0, alpha: 1.0)
+	}
+}
---
+++ gift/UI/UIFont+GFTAdditions.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont+GFTAdditions.swift
+//
+//  Created by Matan Lachmish on 06/06/2016.
+//  Copyright © 2016 GiftApp. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+	class func gftHeader1Font() -> UIFont? {
+		return UIFont(name: "Rubik-Light", size: 25.0)
+	}
+
+	class func gftLinkFont() -> UIFont? {
+		return UIFont(name: "Rubik-Regular", size: 15.0)
+	}
+
+	class func gftText1Font() -> UIFont? {
+		return UIFont(name: "Rubik-Light", size: 15.0)
+	}
+}
