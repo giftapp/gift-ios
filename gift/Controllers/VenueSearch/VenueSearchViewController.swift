294fc9ff2fac52773dca3554a42b9c2d0664352d
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-12-23 09:26:50 +0200

    Remove cancel button from venue search

--- gift/Controllers/VenueSearch/VenueSearchViewController.swift
+++ gift/Controllers/VenueSearch/VenueSearchViewController.swift
@@ -93,11 +93,6 @@ class VenueSearchViewController: UIViewController, UISearchResultsUpdating, UITa
         self.navigationController!.navigationBar.barTintColor = UIColor.gftWaterBlueColor()
         self.navigationController!.navigationBar.tintColor = UIColor.gftWhiteColor()
         self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont.gftNavigationTitleFont()!, NSForegroundColorAttributeName: UIColor.gftWhiteColor()]
-
-        let cancelBarButtonItem = UIBarButtonItem(title: "NavigationViewController.Cancel".localized, style: .plain, target: self, action: #selector(didTapCancel))
-        cancelBarButtonItem.tintColor = UIColor.gftWhiteColor()
-        cancelBarButtonItem.setTitleTextAttributes([NSFontAttributeName: UIFont.gftNavigationItemFont()!, NSForegroundColorAttributeName: UIColor.gftWhiteColor()], for: .normal)
-        self.navigationItem.rightBarButtonItem = cancelBarButtonItem
     }
 
     private func updateCustomViews() {
